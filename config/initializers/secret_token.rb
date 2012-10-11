7c37edf16c43448af7295c2cb9ae1f1b722a0ac6
Michael Heu <mheu@clickbooq.com>
2012-10-11 17:38:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minitest::Application.config.secret_token = '06a7d0e25d8f4b1dc051baf32e0c7f30b9324d35a9053191f29b56f43415bba86215eda6d66cd5a8399e1235e1a32aa9d1bf7a704e66459524cebf366b276c8c'
